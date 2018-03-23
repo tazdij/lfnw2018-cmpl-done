b8293e62d91290a3466871ccadd2084c902d07f4
Don Duvall (Tazdij) <shout@tazdij.com>
2018-03-23 09:35:06 -0700

    updating some suff

--- example/test.s
+++ example/test.s
@@ -1,9 +1,9 @@
 ; Should the source fix endianess?
 ; FEED = ED FE 00 00 ; in memory
 
-MOV     @x0100  xFEED
+MOV     @x0100  xFEED           ; jksljflkjdslkajfdlksa f
 MOV     @x0104  @x0100  x04
 MOV     @x0108  x41
-MOV     R1      x41
+;MOV     R1      x41
 PRINTC  @x0108
 HALT
